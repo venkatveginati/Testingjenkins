3e5a03351fb71e555e303464eebe201a22643ff8
venkatveginati <95076345+venkatveginati@users.noreply.github.com>
2022-01-12 12:03:48 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "runnning the script...!!"
+echo "------------------------------!"
+OS=$(cat /etc/lsb-release)
+echo "the os family details are: $OS"
+echo "------------------------------!"
